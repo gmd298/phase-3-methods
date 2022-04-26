0adeebe613d11ac10c7a20ef2033894ba323f759
Griffin <gmd298@gmail.com>
2022-04-26 11:38:20 -0400

    commit tree c9ebd232bb7ae54889607e0f47fdd93731671c5e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+sum1 = add(1, 2)
+
+def halve(number)
+  if number.class == String
+    return nil
+  end
+
+  number / 2
+end
+
+
